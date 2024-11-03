4a03fffd6f752b18e2cfb582d398aab9762a0b27
AndrewM <amkmathai@gmail.com>
2024-11-03 15:53:50 -0500

    Files from 2023-2024

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lastyear/ArmTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lastyear/ArmTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.lastyear;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
